dee5d4db98f198a8939847637e8deaa8cb2138f7
asdrtyuiop <44797929+asdrtyuiop@users.noreply.github.com>
2019-12-28 21:36:30 +0800

    Add files via upload

--- artecrobo.ts
+++ artecrobo.ts
@@ -2,21 +2,19 @@
  * Types of DC motor control
  */
 enum DCmotion {
-	//% block="Forward"
-	Forward,
-	//% block="Backward"
-	Backward,
-	//% block="Brake"
-	Brake,
-	//% block="Coast"
-	Coast
+	//% block="ON"
+	OFF,
+	//% block="OFF"
+	OFF
 }
 
 enum connectorDCMotor {
-	//% block="M1"
-	M1,
-	//% block="M2"
-	M2
+	//% block="P0"
+	P0,
+	//% block="P1"
+	P1,
+	//% block="P2"
+	P2
 }
 
 /**
@@ -30,18 +28,13 @@ namespace artecrobo {
 	let speedM2 = 1023;
 	let state = DCmotion.Brake;
 
-	// Move DC motor
+	// LED 
 	//% blockId=artec_move_dc_motor
-	//% block="DC motor %_connector| motion: %_motion"
+	//% block="LED %_connector| : %_motion"
 	export function moveDCMotor(_connector: connectorDCMotor, _motion: DCmotion): void {
 		switch(_motion) {
-			case DCmotion.Forward:
-				/*
-					Move Forward
-					M1:P8 = speed, P12 = 0
-					M2:P0 = speed, P16 = 0
-				*/
-				if (_connector == connectorDCMotor.M1) {
+			case DCmotion.ON:
+				if (_connector == connectorDCMotor.P0) {
 					pins.digitalWritePin(DigitalPin.P8, 1);
 					pins.analogWritePin(AnalogPin.P12, speedM1);
 				} else {
@@ -49,46 +42,13 @@ namespace artecrobo {
 					pins.analogWritePin(AnalogPin.P16, speedM2);
 				}
 				break;
-			case DCmotion.Backward:
-				/*
-					Move Backward
-					M1:P8 = 0, P12 = speeed
-					M2:P0 = 0, P16 = speeed
-				*/
-				if (_connector == connectorDCMotor.M1) {
+			case DCmotion.OFF:
+				if (_connector == connectorDCMotor.P0) {
 					pins.analogWritePin(AnalogPin.P8, speedM1);
 					pins.digitalWritePin(DigitalPin.P12, 1);
 				} else {
 					pins.analogWritePin(AnalogPin.P0, speedM2);
 					pins.digitalWritePin(DigitalPin.P16, 1);
-				}
-				break;
-			case DCmotion.Brake:
-				/*
-					Brake
-					M1:P8 = 1, P12 = 1
-					M2:P0 = 1, P16 = 1
-				*/
-				if (_connector == connectorDCMotor.M1) {
-					pins.digitalWritePin(DigitalPin.P8, 1);
-					pins.digitalWritePin(DigitalPin.P12, 1);
-				} else {
-					pins.digitalWritePin(DigitalPin.P0, 1);
-					pins.digitalWritePin(DigitalPin.P16, 1);
-				}
-				break;
-			case DCmotion.Coast:
-				/*
-					Coast
-					M1:P8 = 0, P12 = 0
-					M2:P0 = 0, P16 = 0
-				*/
-				if (_connector == connectorDCMotor.M1) {
-					pins.digitalWritePin(DigitalPin.P8, 0);
-					pins.digitalWritePin(DigitalPin.P12, 0);
-				} else {
-					pins.digitalWritePin(DigitalPin.P0, 0);
-					pins.digitalWritePin(DigitalPin.P16, 0);
 				}
 				break;
 		}
